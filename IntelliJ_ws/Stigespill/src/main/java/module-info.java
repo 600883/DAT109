c43f8d698254eb41fcfca89f71952b8fb67fcbb5
600883 <sivertseter@yahoo.com>
2023-01-31 17:59:43 +0100

    commit stigespill obligen

--- IntelliJ_ws/Stigespill/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module no.hvl.dat109.stigespill {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens no.hvl.dat109.stigespill to javafx.fxml;
-    exports no.hvl.dat109.stigespill;
-}
